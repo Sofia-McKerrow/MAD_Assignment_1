0ea0db03852eaf9ccf108c02c0555c3729d8afd4
rmit-s3626804-sofia-mckerrow <s3626804@student.rmit.edu.au>
2018-08-29 16:18:09 +1000

    Added trackable name and image to DisplayTrackableRouteInfoActivity

--- app/src/main/java/au/edu/rmit/mckerrow/sofia/mad_assignment_1/controller/DisplayTrackableRouteInfoActivity.java
+++ app/src/main/java/au/edu/rmit/mckerrow/sofia/mad_assignment_1/controller/DisplayTrackableRouteInfoActivity.java
@@ -1,9 +1,14 @@
 package au.edu.rmit.mckerrow.sofia.mad_assignment_1.controller;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +18,9 @@ import au.edu.rmit.mckerrow.sofia.mad_assignment_1.model.ReadFile;
 import au.edu.rmit.mckerrow.sofia.mad_assignment_1.model.TrackableInfo;
 
 public class DisplayTrackableRouteInfoActivity extends AppCompatActivity {
+
+    private TextView trackableName;
+    private ImageView trackableImage;
 
     private static List<BirdTrackable> trackableList;
     private static Map<String, BirdTrackable> trackableMap;
@@ -30,10 +38,30 @@ public class DisplayTrackableRouteInfoActivity extends AppCompatActivity {
 
         trackableList = trackableInfo.getTrackableList();
         trackableMap = trackableInfo.getTrackableMap();
-
         String trackableID = getIntent().getExtras().getString(TrackableAdapter.TRACKABLE_ID_KEY);
         BirdTrackable birdTrackable = trackableMap.get(trackableID);
         Toast.makeText(this, "Received item " + birdTrackable.getName(), Toast.LENGTH_SHORT).show();
 
+        trackableName = (TextView) findViewById(R.id.trackableName);
+        trackableImage = (ImageView) findViewById(R.id.trackableImage);
+        trackableName.setText(birdTrackable.getName());
+
+        InputStream inputStream = null;
+        try {
+            String imageFile = birdTrackable.getImage();
+            inputStream = getAssets().open(imageFile);
+            Drawable d = Drawable.createFromStream(inputStream, null);
+            trackableImage.setImageDrawable(d);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
