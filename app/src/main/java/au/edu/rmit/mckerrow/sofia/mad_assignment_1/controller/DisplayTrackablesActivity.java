de62cac4467bfc3a96f008f7103cbbbf275e32c7
rmit-s3626804-sofia-mckerrow <s3626804@student.rmit.edu.au>
2018-08-31 21:40:37 +1000

    Removed action bar from TabWidgetActivity so that it doesn't show below the tabs

--- app/src/main/java/au/edu/rmit/mckerrow/sofia/mad_assignment_1/controller/DisplayTrackablesActivity.java
+++ app/src/main/java/au/edu/rmit/mckerrow/sofia/mad_assignment_1/controller/DisplayTrackablesActivity.java
@@ -1,5 +1,6 @@
 package au.edu.rmit.mckerrow.sofia.mad_assignment_1.controller;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
